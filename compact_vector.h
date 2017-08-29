9b4ce1a22424887d11c1578d0300f0b2167a0f87
Ivan Pleshkov <pleshkov.ivan@gmail.com>
2017-08-29 15:08:17 +0300

    most of all without testing

--- compact_vector.h
+++ compact_vector.h
@@ -7,6 +7,7 @@
 #include <utility>
 #include <limits>
 #include <type_traits>
+#include <algorithm>
 
 template <
 	class T,
@@ -33,7 +34,9 @@ public:
 
 	static constexpr size_t compact_default_capacity = sizeof(full_storage) / sizeof(T);
 
-	static constexpr size_t compact_capacity = compact_max_size < 0 ? compact_default_capacity : compact_max_size;
+	static constexpr size_t compact_default_capacity_nonzero = compact_default_capacity == 0 ? 1 : compact_default_capacity;
+
+	static constexpr size_t compact_capacity = compact_max_size <= 0 ? compact_default_capacity_nonzero : compact_max_size;
 
 	static constexpr size_t vector_max_size = std::numeric_limits<size_t>::max() >> 1;
 
@@ -55,18 +58,18 @@ public:
 	// Empty Base Optimization
 	struct size_allocator_pair : public allocator_type
 	{
+		// bitset 1000...000
+		static const size_t zero_compact = size_t(1) << (8 * sizeof(size_t) - 1);
+
 		size_allocator_pair(const allocator_type& base) :
-			size(0),
 			allocator_type(base)
 		{}
 
 		size_allocator_pair(allocator_type&& base) :
-			size(0),
 			allocator_type(base)
 		{}
 
 		size_allocator_pair() :
-			size(0),
 			allocator_type(allocator_type())
 		{}
 
@@ -87,7 +90,7 @@ public:
 
 		bool is_compact() const noexcept
 		{
-			return !(bool)(size >> (8 * sizeof(size_t) - 1));
+			return size & zero_compact;
 		}
 
 		void set_size(size_t new_size, bool is_compact)
@@ -96,14 +99,14 @@ public:
 			if (new_size > vector_max_size)
 				throw std::exception(u8"попытка создать вектор больше max_size");
 #endif
-
-			size = !is_compact;
-			size = size << (8 * sizeof(size_t) - 1);
-			size = size | new_size;
+			if (is_compact)
+				size = zero_compact | new_size;
+			else
+				size = new_size;
 		}
 
 	private:
-		size_t size;
+		size_t size = zero_compact;
 	};
 
 	union
@@ -158,7 +161,7 @@ public:
 	compact_vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) :
 		size_allocaltor(alloc)
 	{
-		// insert(begin(), first, last);
+		assign(first, last);
 	}
 
 	/// constructor: copy
@@ -168,7 +171,8 @@ public:
 	compact_vector(const this_type& x) :
 		size_allocaltor(x.get_allocator())
 	{
-		
+		reserve(x.size());
+		copy_data(x.begin(), x.end(), begin());
 	}
 
 	/// constructor: copy
@@ -178,7 +182,8 @@ public:
 	compact_vector(const this_type& x, const allocator_type& alloc) :
 		size_allocaltor(alloc)
 	{
-		// todo
+		reserve(x.size());
+		copy_data(x.begin(), x.end(), begin());
 	}
 
 	/// constructor: move
@@ -214,7 +219,7 @@ public:
 	compact_vector(std::initializer_list<T> il, const allocator_type& alloc = allocator_type()) :
 		size_allocaltor(alloc)
 	{
-		insert(begin(), il);
+		assign(il);
 	}
 
 	/// destructor
@@ -361,12 +366,32 @@ public:
 	iterator erase(const_iterator position)
 	{
 		iterator p = const_cast<iterator>(position);
+		iterator e = end();
 
-		// todo
+		if (p <= e)
+			copy_data(p + 1, e, p); // todo: переделать на семантику перемещения
+		e--;
+		e->~T();
+		set_new_size(size() - 1);
 		return p;
 	}
 
-	iterator erase(const_iterator first, const_iterator last);
+	iterator erase(const_iterator first, const_iterator last)
+	{
+		iterator f = const_cast<iterator>(first);
+		iterator l = const_cast<iterator>(last);
+		iterator e = end();
+
+		if (f > l)
+			return nullptr;
+
+		if (l <= e)
+			copy_data(l, e, f);  // todo: переделать на семантику перемещения
+		call_destructors(f + (e - l), e);
+
+		set_new_size(size() - (e - l));
+		return f;
+	}
 
 	T& front()
 	{
@@ -384,20 +409,63 @@ public:
 	}
 
 	/// insert: single element
-	iterator insert(const_iterator position, const T& val); // todo
+	iterator insert(const_iterator position, const T& val)
+	{
+		return insert(position, 1, val);
+	}
 
 	/// insert: fill
-	iterator insert(const_iterator position, size_t n, const T& val); // todo
+	iterator insert(const_iterator position, size_t n, const T& val)
+	{
+		size_t p = end() - position;
+		resize(size() + n, val);
+
+		iterator i = end() - n - 1;
+		iterator j = end() - 1;
+		for (; p > 0; p--, i--, j--)
+			std::iter_swap(i, j);
+		return i;
+	}
 
 	/// insert: range
 	template <class InputIterator>
-	iterator insert(const_iterator position, InputIterator first, InputIterator last); // todo
+	iterator insert(const_iterator position, InputIterator first, InputIterator last)
+	{
+		if (first == last)
+			return end();
+		// todo
+		return nullptr;
+	}
 
 	/// insert: move
-	iterator insert(const_iterator position, T&& val); // todo
+	iterator insert(const_iterator position, T&& val)
+	{
+		return emplace(position, std::move(value));
+	}
 
 	/// initializer list
-	iterator insert(const_iterator position, std::initializer_list<T> il); // todo
+	iterator insert(const_iterator position, std::initializer_list<T> il)
+	{
+		if (il.size() == 0)
+			return end();
+
+		size_t p = end() - position;
+		resize(size() + il.size(), *il.begin());
+
+		iterator i = end() - il.size() - 1;
+		iterator j = end() - 1;
+		for (size_t counter = p; counter > 0; counter--, i--, j--)
+			std::iter_swap(i, j);
+
+		i = begin() + p;
+		for (auto& e : il)
+		{
+			*i = e;
+			i++;
+		}
+
+		return begin() + p;
+	}
 
 	size_t max_size() const noexcept
 	{
@@ -405,13 +473,36 @@ public:
 	}
 
 	// operator=, copy
-	compact_vector& operator= (const compact_vector& x);
+	compact_vector& operator= (const compact_vector& x)
+	{
+		if (this != &x)
+		{
+			clear();
+			reserve(x.size());
+			copy_data(x.begin(), x.end(), begin());
+		}
+		return *this;
+	}
 
 	// operator=, move
-	compact_vector& operator= (compact_vector&& x);
+	compact_vector& operator= (compact_vector&& x)
+	{
+		if (this != &x)
+		{
+			destruct();
+			swap(x);
+		}
+		return *this;
+	}
 
 	// operator=, initializer list
-	compact_vector& operator= (std::initializer_list<T> il);
+	compact_vector& operator= (std::initializer_list<T> il)
+	{
+		clear();
+		reserve(il.size());
+		copy_data(il.begin(), il.end(), begin());
+		return *this;
+	}
 
 	T& operator[] (size_t n)
 	{
@@ -450,7 +541,7 @@ public:
 		if (size > capacity())
 			reserve(2 * capacity());
 
-		::new(end()) T(val);
+		::new(end()) T(std::move(val));
 		set_new_size(size);
 	}
 
@@ -485,7 +576,7 @@ public:
 		else
 		{
 			call_destructors(begin() + n, end());
-			this->size_allocaltor.set_size(n, is_compact());
+			set_new_size(n);
 		}
 	}
 
@@ -503,13 +594,36 @@ public:
 		else
 		{
 			call_destructors(begin() + n, end());
-			this->size_allocaltor.set_size(n, is_compact());
+			set_new_size(n);
 		}
 	}
 
 	void shrink_to_fit()
 	{
-		// todo
+		if (is_compact())
+			return;
+
+		if (size() >= compact_capacity)
+		{
+			size_t new_capacity = size();
+			auto ptr_begin = get_allocator().allocate(new_capacity);
+			move_data(begin(), end(), ptr_begin);
+
+			get_allocator().deallocate(full.begin, full.capacity);
+
+			full.begin = ptr_begin;
+			full.capacity = new_capacity;
+		}
+		else
+		{
+			auto b = full.begin;
+			auto c = full.capacity;
+
+			move_data(b, b + c, compact.get(0));
+			get_allocator().deallocate(b, c);
+
+			size_allocaltor.set_size(size(), true);
+		}
 	}
 
 	size_t size() const noexcept
@@ -519,11 +633,27 @@ public:
 
 	void swap(compact_vector& x)
 	{
-		// todo
+		if (is_compact())
+		{
+			if (x.is_compact())
+				swap_compact_compact(x);
+			else
+				swap_compact_full(x);
+		}
+		else
+		{
+			if (x.is_compact())
+				swap_full_compact(x);
+			else
+				swap_full_full(x);
+		}
 	}
 
-//private:
+#ifdef COMPACT_VECTOR_DEBUG
 public:
+#else
+private:
+#endif
 
 	bool is_compact() const noexcept
 	{
@@ -537,6 +667,103 @@ public:
 		if (!is_compact())
 			get_allocator().deallocate(full.begin, full.capacity);
 		size_allocaltor.set_size(0, true);
+	}
+
+	// swap для случая, когда this->is_compact() == false && x.is_compact() == false
+	void swap_full_full(this_type& x)
+	{
+#ifdef COMPACT_VECTOR_DEBUG
+		if (is_compact() != false || x.is_compact() != false)
+			throw std::exception(u8"неправильный вызов swap_full_full");
+#endif // COMPACT_VECTOR_DEBUG
+
+		std::swap(size_allocaltor, x.size_allocaltor);
+		std::swap(full.begin, x.full.begin);
+		std::swap(full.capacity, x.full.capacity);
+	}
+
+	// swap для случая, когда this->is_compact() == true && x.is_compact() == false
+	void swap_compact_full(this_type& x)
+	{
+#ifdef COMPACT_VECTOR_DEBUG
+		if (is_compact() != true || x.is_compact() != false)
+			throw std::exception(u8"неправильный вызов swap_compact_full");
+#endif // COMPACT_VECTOR_DEBUG
+
+		auto x_begin = x.full.begin;
+		auto x_capacity = x.full.capacity;
+
+		move_data(begin(), end(), x.compact.get(0));
+		full.begin = x_begin;
+		full.capacity = x_capacity;
+
+		std::swap(size_allocaltor, x.size_allocaltor);
+	}
+
+	// swap для случая, когда this->is_compact() == false && x.is_compact() == true
+	void swap_full_compact(this_type& x)
+	{
+#ifdef COMPACT_VECTOR_DEBUG
+		if (is_compact() != false || x.is_compact() != true)
+			throw std::exception(u8"неправильный вызов swap_full_compact");
+#endif // COMPACT_VECTOR_DEBUG
+
+		auto this_begin = full.begin;
+		auto this_capacity = full.capacity;
+
+		move_data(x.begin(), x.end(), compact.get(0));
+		x.full.begin = x_begin;
+		x.full.capacity = x_capacity;
+
+		std::swap(size_allocaltor, x.size_allocaltor);
+	}
+
+	// swap для случая, когда this->is_compact() == true && x.is_compact() == true
+	void swap_compact_compact(this_type& x)
+	{
+#ifdef COMPACT_VECTOR_DEBUG
+		if (is_compact() != true || x.is_compact() != true)
+			throw std::exception(u8"неправильный вызов swap_compact_compact");
+#endif // COMPACT_VECTOR_DEBUG
+
+		swap_compact_compact(x, std::is_trivially_copyable<T>::type());
+	}
+
+	void swap_compact_compact(this_type& x, std::integral_constant<bool, true>)
+	{
+		compact_storage tmp;
+		std::memcpy(&tmp, &compact, sizeof(compact_storage));
+		std::memcpy(&compact, &x.compact, sizeof(compact_storage));
+		std::memcpy(&x.compact, &tmp, sizeof(compact_storage));
+		
+		std::swap(size_allocaltor, x.size_allocaltor);
+	}
+
+	void swap_compact_compact(this_type& x, std::integral_constant<bool, false>)
+	{
+		size_t s1 = size();
+		size_t s2 = x.size();
+		size_t s = std::max(s1, s2);
+
+		for (size_t i = 0; i < s; i++)
+		{
+			if (i < s1 && i < s2)
+			{
+				std::iter_swap(compact.get(i), x.compact.get(i));
+			}
+			else if (i < s2)
+			{
+				::new(compact.get(i)) T(std::move(*x.compact.get(i)));
+				x.compact.get(i)->~T();
+			}
+			else
+			{
+				::new(x.compact.get(i)) T(std::move(*compact.get(i)));
+				compact.get(i)->~T();
+			}
+		}
+
+		std::swap(size_allocaltor, x.size_allocaltor);
 	}
 
 	template<typename InputIterator>
@@ -556,9 +783,7 @@ public:
 	static void call_destructors(InputIterator first, InputIterator last, std::integral_constant<bool, false>)
 	{
 		for (; first != last; first++)
-		{
 			first->~T();
-		}
 	}
 
 	void grow(size_t new_size)
@@ -571,7 +796,6 @@ public:
 		auto ptr_begin = get_allocator().allocate(new_size);
 		move_data(begin(), end(), ptr_begin);
 
-		call_destructors(begin(), end());
 		if (!is_compact())
 			get_allocator().deallocate(full.begin, full.capacity);
 
@@ -630,9 +854,8 @@ public:
 	static void move_data(iterator first, iterator last, iterator target, std::integral_constant<bool, false>)
 	{
 		for (; first != last; first++, target++)
-		{
-			*target = std::move(*first);
-		}
+			::new(target) T(std::move(*first));
+		call_destructors(first, last);
 	}
 
 	static void copy_data(const_iterator first, const_iterator last, iterator target)
@@ -646,7 +869,7 @@ public:
 		std::memcpy(target, first, last - first);
 	}
 
-	// для нетривиальных типов вызывается std::move
+	// для нетривиальных типов вызывается поэлементное копирование
 	static void copy_data(const_iterator first, const_iterator last, iterator target, std::integral_constant<bool, false>)
 	{
 		for (; first != last; first++, target++)
@@ -655,3 +878,4 @@ public:
 		}
 	}
 };
+
